1e055a30786defa86a80df9af2773fcbacca1e64
Matias <mquezadadev@gmail.com>
2020-12-29 23:45:36 -0300

    add config files to api rest & create api rest to get answers

---
+++ application/controllers/api/Answers.php
@@ -0,0 +1,34 @@
+<?php
+   
+require APPPATH . 'libraries/REST_Controller.php';
+     
+class Answers extends REST_Controller {
+    
+	  /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function __construct() {
+       parent::__construct();
+       $this->load->database();
+    }
+       
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+	public function index_get($id = 0)
+	{
+        if(!empty($id)){
+            $data = $this->db->get_where("answers", ['id' => $id])->row_array();
+        }else{
+            $data = $this->db->get("answers")->result();
+        }
+     
+        $this->response($data, REST_Controller::HTTP_OK);
+	}
+    	
+}
+?>
